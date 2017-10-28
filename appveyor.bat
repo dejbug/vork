d95d916eb0cef2d4a52730078c9e32bb46d4cb52
Dejan Budimir <dejbug@users.noreply.github.com>
2017-10-28 20:33:49 +0200

    Update appveyor.bat

--- appveyor.bat
+++ appveyor.bat
@@ -1,14 +1,16 @@
-@echo off
+@ECHO off
 
-echo %APPVEYOR_API_URL%
+ECHO %APPVEYOR_API_URL%
 
-set PATH=C:\mingw-w64\i686-6.3.0-posix-dwarf-rt_v5-rev1\mingw32\bin;%PATH%
+SET MINGW_BIN=C:\mingw-w64\i686-6.3.0-posix-dwarf-rt_v5-rev1\mingw32\bin
+SET WGET_BIN=c:\MinGW\msys\1.0\bin
+SET PATH=%MINGW_BIN%;%WGET_BIN%;%PATH%
 
 mingw32-make --version | head -1
 g++ --version | head -1
 ar --version | head -1
 
-mkdir build
-mkdir deploy
+MKDIR build
+MKDIR deploy
 
 mingw32-make -j1 --keep-going SHELL=cmd.exe
